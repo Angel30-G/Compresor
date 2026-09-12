92c1e6db132e3b00d4517feda325ae43ac177af2
KeylorH <keylor.herrera.fuentes@gmail.com>
2026-09-12 09:34:50 -0600

    Implementa conteo de frecuencias desde archivo real

--- src/main.c
+++ src/main.c
@@ -13,26 +13,43 @@ int main() {
         printf("No se pudo calcular MD5\n");
     }
 
-    // Probar Huffman: construir árbol con frecuencias de ejemplo
-    unsigned long freq[256] = {0};
-    freq['a'] = 5;
-    freq['b'] = 9;
-    freq['c'] = 12;
-    freq['d'] = 13;
-    freq['e'] = 16;
-    freq['f'] = 45;
+    // Probar Huffman: construir arbol con frecuencias de ejemplo
+    unsigned long freq[256];
+    unsigned long long total_bytes = 0;
+
+    if (count_frequencies("tests/prueba.txt", freq, &total_bytes) != 0) {
+    	printf("Error al leer el archivo de prueba\n");
+    	return 1;
+   }
+
+    printf("\nTotal de bytes: %llu\n", total_bytes);
+    printf("Frecuencias encontradas:\n");
+
+    for (int i = 0; i < 256; i++) {
+    	if (freq[i] > 0) {
+        	if (i >= 32 && i <= 126) {
+            		printf("'%c': %lu\n", i, freq[i]);
+        	} else {
+            		printf("Byte %d: %lu\n", i, freq[i]);
+        	}
+    	}
+   }
 
     Node *root = build_huffman_tree(freq);
     char *codes[256] = {0};
     char buffer[256];
     generate_codes(root, codes, buffer, 0);
 
-    for (int i = 0; i < 256; i++) {
-        if (codes[i]) {
+   for (int i = 0; i < 256; i++) {
+    if (codes[i]) {
+        if (i >= 32 && i <= 126) {
             printf("'%c': %s\n", i, codes[i]);
-            free(codes[i]);
+        } else {
+            printf("Byte %d: %s\n", i, codes[i]);
         }
+
+        free(codes[i]);
     }
-    free_tree(root);
-    return 0;
+}
+
 }
