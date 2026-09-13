a8119a5354b505b1c942724a3fea5e8dbc8e6b1b
KeylorH <keylor.herrera.fuentes@gmail.com>
2026-09-13 00:27:23 -0600

    Actualiza MD5 a API EVP de OpenSSL

--- src/md5_util.c
+++ src/md5_util.c
@@ -1,26 +1,107 @@
+#include <stdio.h>
+
 #include "md5_util.h"
 
-int compute_md5(const char *filename, unsigned char *md5_result) {
-    FILE *f = fopen(filename, "rb");
-    if (!f) return -1;
+int compute_md5(
+    const char *filename,
+    unsigned char *md5_result
+) {
+    FILE *file = fopen(filename, "rb");
 
-    MD5_CTX ctx;
-    MD5_Init(&ctx);
-
-    unsigned char buffer[8192];
-    size_t bytes;
-    while ((bytes = fread(buffer, 1, sizeof(buffer), f)) > 0) {
-        MD5_Update(&ctx, buffer, bytes);
+    if (!file) {
+        return -1;
     }
 
-    MD5_Final(md5_result, &ctx);
-    fclose(f);
+    EVP_MD_CTX *ctx = EVP_MD_CTX_new();
+
+    if (!ctx) {
+        fclose(file);
+        return -1;
+    }
+
+    const EVP_MD *md = EVP_md5();
+
+    if (!md) {
+        EVP_MD_CTX_free(ctx);
+        fclose(file);
+        return -1;
+    }
+
+    if (EVP_DigestInit_ex(ctx, md, NULL) != 1) {
+        EVP_MD_CTX_free(ctx);
+        fclose(file);
+        return -1;
+    }
+
+    unsigned char buffer[8192];
+    size_t bytes_read;
+
+    while (
+        (
+            bytes_read = fread(
+                buffer,
+                1,
+                sizeof(buffer),
+                file
+            )
+        ) > 0
+    ) {
+        if (
+            EVP_DigestUpdate(
+                ctx,
+                buffer,
+                bytes_read
+            ) != 1
+        ) {
+            EVP_MD_CTX_free(ctx);
+            fclose(file);
+            return -1;
+        }
+    }
+
+    if (ferror(file)) {
+        EVP_MD_CTX_free(ctx);
+        fclose(file);
+        return -1;
+    }
+
+    unsigned int digest_length = 0;
+
+    if (
+        EVP_DigestFinal_ex(
+            ctx,
+            md5_result,
+            &digest_length
+        ) != 1
+    ) {
+        EVP_MD_CTX_free(ctx);
+        fclose(file);
+        return -1;
+    }
+
+    EVP_MD_CTX_free(ctx);
+    fclose(file);
+
+    if (digest_length != MD5_DIGEST_LENGTH) {
+        return -1;
+    }
+
     return 0;
 }
 
-void md5_to_hex(const unsigned char *md5, char *hex_output) {
+void md5_to_hex(
+    const unsigned char *md5,
+    char *output
+) {
     for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
-        sprintf(hex_output + i*2, "%02x", md5[i]);
+        sprintf(
+            &output[i * 2],
+            "%02x",
+            md5[i]
+        );
     }
-    hex_output[MD5_DIGEST_LENGTH*2] = '\0';
+
+    output[
+        MD5_DIGEST_LENGTH * 2
+    ] = '\0';
 }
